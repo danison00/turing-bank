c190d2c079524c668a58af4a0815ab1f43908356
Dânison dos Santos <danison.santos00@gmail.com>
2023-11-27 00:56:55 -0300

    up

--- src/main/java/dan/turingbank/web/viewControllers/HomeController.java
+++ src/main/java/dan/turingbank/web/viewControllers/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping()
 public class HomeController {
     
-    @GetMapping("/home")
+    @GetMapping("home")
     public String home(Authentication authentication){
         return "index";
     }
