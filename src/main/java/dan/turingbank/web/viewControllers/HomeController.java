161a930591fae95c14723bcb4e955b2182ca11be
Dânison dos Santos <danison.santos00@gmail.com>
2023-11-27 00:41:26 -0300

    up

--- src/main/java/dan/turingbank/web/viewControllers/HomeController.java
+++ src/main/java/dan/turingbank/web/viewControllers/HomeController.java
@@ -3,16 +3,22 @@ package dan.turingbank.web.viewControllers;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping()
 public class HomeController {
     
-    @GetMapping(value = "/home")
+    @GetMapping("/home")
     public String home(Authentication authentication){
         return "index";
     }
-    @GetMapping({"/"})
+    @GetMapping({"/home/**"})
     public String raiz(Authentication authentication){
+        return "redirect:/home";
+    }
+    @GetMapping("/login")
+    public String login(Authentication authentication){
         return "redirect:/home";
     }
 
